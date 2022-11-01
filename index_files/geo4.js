897ab7740fab17f98005f7ea8771645878441d20
teeowo <owobotosin@yahoo.com>
2022-11-01 16:27:17 -0600

    added index files

---
+++ index_files/geo4.js
@@ -0,0 +1,17 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "CALGARY",
+      'continent': "NA",
+      'country': "CA",
+      'region': "AB",
+      'dma': ""
+    },
+    'ip':"70.77.158.126"
+  }]);
+})
+//
+()
+
+;
