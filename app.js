08492655c264f0fc98abed1c3b7a515717723126
Ubuntu <ubuntu@ip-172-31-42-145.us-east-2.compute.internal>
2022-11-08 21:55:47 +0000

    added the app.js for Nodejs

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile( __dirname + "/index.html");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
